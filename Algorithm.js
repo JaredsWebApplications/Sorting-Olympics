58a87b4750024829aeb1ee9c7bd0fabb3223730b
OverworldLord <36016717+OverworldLord@users.noreply.github.com>
2020-10-13 13:07:00 -0700

    Add algorithm.js

---
+++ Algorithm.js
@@ -0,0 +1,61 @@
+/************************************************************************
+
+ *************************************************************************/
+
+const SORT = {
+	INSERTION 	: 0,
+	PORE		: 1,
+	MERGE		: 2,
+	QUICK		: 3
+}
+
+class Algorithm {
+	constructor() {
+		this.InsertionSort 	= 0;
+		this.PoreSort		= 0;
+		this.MergeSort		= 0;
+		this.QuickSort		= 0;	
+	}
+	Pass(stringSorting, sortType) {
+		//Call the sorting algorithm we want in the following switch statement
+		switch(sortType)
+		{
+			case SORT.INSERTION:
+				
+				break;
+			case SORT.PORT:
+				
+				break;
+			case SORT.MERGE:
+				
+				break;
+			case SORT.QUICK:
+				
+				break;
+		}
+		return stringSorting;
+	}
+	
+	UpdateDraw(stringDisplaying, index) {
+		//We're going to need to output in a different location based on the index
+		switch(index)
+		{
+			case SORT.INSERTION:
+				
+				break;
+			case SORT.PORT:
+				
+				break;
+			case SORT.MERGE:
+				
+				break;
+			case SORT.QUICK:
+				
+				break;
+		}
+	}
+	
+	Sort() {
+		//Fully sorts a string using any of the algorithms we have, just so we know what we're expecting as a result when we actually care to display
+	}
+}
