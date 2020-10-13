158ed69765b5f33d5affa0d1aa7962f049ec5ed7
OverworldLord <36016717+OverworldLord@users.noreply.github.com>
2020-10-12 18:21:07 -0700

    Fix dumb JS mistakes

--- Display.js
+++ Display.js
@@ -2,22 +2,20 @@
 
  *************************************************************************/
 
+const ORIGINAL_STRING = "5F7D8A1593B47B8";				//Initial string
 
-const var NUM_ALGOS = 4;							//Number of algorithms we're sorting with
-const var ORIGINAL_STRING = "5F7D8A1593B47B8";		//Initial string
-
-var passString[NUM_ALGOS] = "";						//An array of strings, each one will be passed into a different algorithm
-var inputString, expectedString = "";				//The original string and the expected string
+var passString = {	ORIGINAL_STRING,
+					ORIGINAL_STRING,
+					ORIGINAL_STRING,
+					ORIGINAL_STRING};					//An array of strings, each one will be passed into a different algorithm
+var inputString, expectedString = "";					//The original string and the expected string
+const NUM_ALGOS = passString.size;						//Number of algorithms we're sorting with
 
 //Function called before draw
 function setup() {
 	//The original string is our inputString
 	inputString = ORIGINAL_STRING;
-	for(int i = 0; i < NUM_ALGOS; i++)
-	{
-		passString[i] = inputString;
-	}
-	//expectedString = Algorithm.Sort();			//Sorts the string completely								NOT IMPLEMENTED
+	//expectedString = Algorithm.Sort();				//Sorts the string completely							NOT IMPLEMENTED
 }
 
 var done = false;
@@ -26,39 +24,39 @@ var done = false;
 function draw() {
 	if(!done)
 	{
-		done = true;								//we're done unless an algorithm updates
+		done = true;									//we're done unless an algorithm updates
 		
 		//Go through all algorithms
-		for(int i = 0; i < NUM_ALGOS; i++)
+		for(let i = 0; i < NUM_ALGOS; i++)
 		{
 			//If the current algorithm isn't done sorting
 			if(passString[i] != expectedString)
 			{
 				//passString[i] = Algorithm.Pass(i);	//Perform a pass on the algorithm						NOT IMPLEMENTED
-				//Algorithm.UpdateDraw(i);			//Draw the update to the screen								NOT IMPLEMENTED
-				done = false;						//An algorithm updated, we're not done yet
+				//Algorithm.UpdateDraw(i);				//Draw the update to the screen							NOT IMPLEMENTED
+				done = false;							//An algorithm updated, we're not done yet
 			}
 		}
 	}
 	else
 	{
 		//Update update our new input string by shifting
-		inputString = inputString.substring(1, substring.length() + inputString.substring(0,0));
+		inputString = inputString.substring(1, inputString.length + inputString.substring(0,0));
 		
 		//If we haven't looped yet
 		if(inputString != ORIGINAL_STRING)
 		{
 			//For every string
-			for(int i = 0; i < NUM_ALGOS; i++)
+			for(let i = 0; i < NUM_ALGOS; i++)
 			{
-				passString[i] = inputString;		//Set the current string to the new input string
+				passString[i] = inputString;			//Set the current string to the new input string
 			}
-			done = false;							//We are no longer done
+			done = false;								//We are no longer done
 		}
 		//else If we've looped, don't update done. We'll stop performing operations.
 		else
 		{
-			noLoop();								//Stops draw from being called again
+			noLoop();									//Stops draw from being called again
 		}
 	}
 }
