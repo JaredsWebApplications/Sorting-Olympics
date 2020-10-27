cf5952c744c82b56834871ae499f115f4dfa9add
OverworldLord <36016717+OverworldLord@users.noreply.github.com>
2020-10-26 21:18:16 -0700

    Implement poresort

--- Algorithm.js
+++ Algorithm.js
@@ -12,7 +12,7 @@ const SORT = {
 class selectAlgorithm {
 	constructor() {
 		this.InsertionIndex = 1;
-		this.PoreSort		= 0;
+		this.PoreSort		= false;
 		this.MergeSort		= 0;
 		this.QuickSort		= 0;
 	}
@@ -23,20 +23,40 @@ class selectAlgorithm {
 		switch(sortType)
 		{
 			case SORT.INSERTION:
+				//If we're not at the end of the string
 				if(this.InsertionIndex < stringSorting.length)
 				{
-					let x = container[this.InsertionIndex];
-					let j = this.InsertionIndex - 1;
+					let x = container[this.InsertionIndex];	//Store the current starting index
+					let j = this.InsertionIndex - 1;		//j stores the index location of the element before our starting index
+					
+					//While we're not out of bounds and we're increasing in size
 					while(j >= 0 && container[j] > x)
 					{
-						container[j + 1] = container[j];
-						j = j - 1;
+						container[j + 1] = container[j];	//push this element back
+						j = j - 1;							//reduce j by 1
 					}
-					container[j + 1] = x;
-					this.InsertionIndex += 1;
+					container[j + 1] = x;					//replace the second smallest element with x
+					this.InsertionIndex += 1;				//Increment insertion index
 				}
 				break;
 			case SORT.PORE:
+				let index 	= 0 + this.PoreSort;	//Decide whether we're sorting the odd or even numbers
+				let temp	= "";					//Used only for swapping
+				
+				//While we're making comparisons inside of bounds
+				while(index + 1 < stringSorting.length)
+				{
+					//If our current value is greater than the next value, swap them
+					if(container[index] > container[index + 1])
+					{
+						temp = container[index + 1];
+						container[index + 1] = container[index];
+						container[index] = temp;
+					}
+					//Move to the next comparison
+					index = index + 2;
+				}
+				this.PoreSort = !this.PoreSort;
 				break;
 			case SORT.MERGE:
 				break;
