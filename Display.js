94fc1cac475e2e96c563206f843eb4257f6ce02f
JaredDyreson <jared.dyreson@gmail.com>
2020-10-28 15:50:29 -0700

    randomly select an element in the list of inputs

--- Display.js
+++ Display.js
@@ -1,4 +1,26 @@
-const ORIGINAL_STRING = "5F7D8A1593B47B8";								//Initial string
+var inputs = [
+    "05CA627BC2B6F03",
+    "065DE6671F040BA",
+    "0684FB893D5754E",
+    "07C9A2D183E4B65",
+    "09F48E7862D2616",
+    "1FAB3D47905C286",
+    "286E1D0342D7859",
+    "30E530C4786AF21",
+    "328DE4765C10BA9",
+    "34F2756F18E90BA",
+    "90BA34F0756F180",
+    "D7859286E2D0342"
+]
+
+function select(container){
+    /*
+     * Source: https://stackoverflow.com/questions/4550505/getting-a-random-value-from-a-javascript-array
+    */
+    return container[Math.floor(Math.random() * container.length)];
+}
+
+var ORIGINAL_STRING = select(inputs);
 
 var passString = new Array(  	ORIGINAL_STRING,									//An array of strings, each one will be passed into a different algorithm
 								ORIGINAL_STRING,
